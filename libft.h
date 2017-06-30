c185a1e881b4f2052fc7529923a67dffeb62e85b
Nick <nekit_p@ukr.net>
2017-06-30 19:09:53 +0300

    putchar returns too

--- libft.h
+++ libft.h
@@ -81,7 +81,7 @@ int				ft_strequ(char const *s1, char const *s2);
 int				ft_strnequ(char const *s1, char const *s2, size_t n);
 char			*ft_strsub(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
-void			ft_putchar(char c);
+ssize_t			ft_putchar(char c);
 ssize_t			ft_putstr(char const *s);
 void			ft_putendl(char const *s);
 void			ft_putnbr(long long n);
