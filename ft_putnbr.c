bb935f23b3277024c60a677003949f7d9f690fd5
Nick <nekit_p@ukr.net>
2017-07-01 21:34:11 +0300

    include limits.h, correct work of putnbr for left side of lon long int

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -17,6 +17,12 @@ size_t	ft_putnbr(long long n)
 	size_t len;
 
 	len = 0;
+	if (n == LLONG_MIN)
+	{
+		len += ft_putchar('-');
+		len += ft_putchar('9');
+		n = 223372036854775808;
+	}
 	if (n < 0)
 	{
 		len += ft_putchar('-');
--- libft.h
+++ libft.h
@@ -24,6 +24,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <string.h>
+# include <limits.h>
 
 typedef struct	s_lost
 {
